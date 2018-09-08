34ca9cd8cc67f36690a8d7dae0ac931bb84b613d
Sina Mahmoodi <itz.s1na@gmail.com>
2018-09-08 11:57:12 +0200

    Add simple form for setting value
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import truffleContract from "truffle-contract";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { value: 0, contractValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,14 +17,13 @@ class App extends Component {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      console.log(SimpleStorageContract)
       const Contract = truffleContract(SimpleStorageContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.fetchValue);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -34,18 +33,28 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
+  fetchValue = async () => {
+    const { contract } = this.state;
 
     // Get the value from the contract to prove it worked.
     const response = await contract.get();
 
     // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
+    this.setState({ contractValue: response.toNumber() });
   };
+
+  onChange = async (e) => {
+    this.setState({ value: e.target.value })
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault()
+    const { accounts, contract } = this.state
+
+    await contract.set(this.state.value, { from: accounts[0] })
+    const response = await contract.get()
+    this.setState({ contractValue: response.toNumber() });
+  }
 
   render() {
     if (!this.state.web3) {
@@ -53,17 +62,16 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Hermes Network</h1>
+        <h2>Try out Hermes the Executor</h2>
+        <form onSubmit={this.onSubmit}>
+          <label>
+            Value:
+            <input type="text" name="value" onChange={this.onChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <div>The on-chain stored value is: {this.state.contractValue}</div>
       </div>
     );
   }
