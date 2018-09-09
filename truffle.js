161ed43a9f873ad5aa2aad43e23c80b30b9b8cad
Sina Mahmoodi <itz.s1na@gmail.com>
2018-09-09 03:50:22 +0200

    Use hdwallet for truffle
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const mnemonic = process.env.MNEMONIC
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +10,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io')
+      },
+      network_id: '3'
     }
   }
 }
